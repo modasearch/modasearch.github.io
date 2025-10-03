b5d9520925d06f0423dc17fefc28da30e93a1cce
pilarczykszczecin-dotcom <pilarczykszczecin@gmail.com>
2025-10-03 14:51:55 +0200

    Initial commit - first version of website

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Strona załadowana pomyślnie!");
+    
+    // Usuwamy poprzednią funkcjonalność kliknięcia, pozostawiamy tylko hover
+    // Animacja jest teraz czysto CSS, więc nie potrzebujemy dodatkowego JavaScript
+});
